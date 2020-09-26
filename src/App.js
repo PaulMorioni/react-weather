831462845f16e4f1cc5abb8d59ef2969c7ece8cb
PaulMorioni <morionipaul@gmail.com>
2020-09-26 16:39:43 -0500

    finished API handeling and display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
 const api = {
-  key: "a", //TODO
-  base: "url", //TODO
+  key: "55d4204c42c1181f449e435b2b25fd6f",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
         .then((result) => {
           setWeather(result);
           setQuery("");
+          console.log(result);
         });
     }
   };
@@ -54,7 +55,15 @@ function App() {
   };
 
   return (
-    <div className="app">
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
           <input
@@ -66,14 +75,22 @@ function App() {
             onKeyPress={search}
           />
         </div>{" "}
-        <div className="location-box">
-          <div className="location"> New York City, US </div>{" "}
-          <div className="date">{dateBuilder(new Date())} </div>{" "}
-        </div>{" "}
-        <div className="weather-box">
-          <div className="temp">15c</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>{" "}
+              <div className="date">{dateBuilder(new Date())} </div>{" "}
+            </div>{" "}
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </main>{" "}
     </div>
   );
